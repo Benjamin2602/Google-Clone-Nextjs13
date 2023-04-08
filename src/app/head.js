c78648905f68f8ef5e4f47edf5fbc9f528f66b29
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-04-08 17:28:53 +0530

    created nextjs app with tailwind css

---
+++ src/app/head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function head() {
+  return (
+  <>
+     <title>Google Clone Next js 13</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created by Next js 13" />
+  </>
+  )
+}
