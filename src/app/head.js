693989ab4ec83e6fdb01c45e3325e21c289911f1
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-04-08 17:34:14 +0530

    included svg file

--- src/app/head.js
+++ src/app/head.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function head() {
+export default function Head() {
   return (
-  <>
-     <title>Google Clone Next js 13</title>
+    <>
+      <title>Google Clone Next js 13</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Google clone created by Next js 13" />
-  </>
-  )
+      <link rel="icon" href="/googlee.svg" />
+    </>
+  );
 }
