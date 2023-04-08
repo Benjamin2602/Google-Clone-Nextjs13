10c1d01c7a93939205a7a5c2ef84f6eb1808fc71
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-04-08 21:34:35 +0530

    Included Google image in the body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
+import Image from "next/image";
 import HomeHeader from "./components/HomeHeader";
 
 
 export default function Home() {
   return (
     <>
+
+
         <HomeHeader/>
+
+        {/* body section */}
+        <div className="flex flex-col items-center mt-24">
+        {/* ability to optimize image in different size */}
+        {/* so this makes the browser to fastly reload the image */}
+          <Image 
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+          alt="Google Logo"
+          
+          ></Image>
+        </div>
     </>
   )
 }
