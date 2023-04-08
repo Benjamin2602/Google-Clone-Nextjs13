feefa3954c553583c10a5c02d7d62a9e2a30ce9f
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-04-08 20:08:19 +0530

    Installed react-icons and styled button

--- src/app/components/HomeHeader.js
+++ src/app/components/HomeHeader.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import {TbGridDots} from "react-icons/tb"
 
 export default function HomeHeader() {
   return (
     <header>
-    <div>
+    <div >
     {/* link tag helps us to go from one page to another without refreshing the page */}
     <Link href="https://mail.google.com"
     className="hover:underline">
@@ -14,11 +15,10 @@ export default function HomeHeader() {
     className="hover:underline">
         Images
     </Link>
-
-
-
+    <TbGridDots className="bg-transparent hover:bg-slate-200 rounded-full text-4xl p-2 hover:shadow-md transition-shadow"/>
+    {/* styled the button  */}
+    <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105">Sign in</button>
     </div>
-      
     </header>
   )
 }
