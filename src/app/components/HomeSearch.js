688cb995c5020f092cc197de04fe71b95986fd40
Jayanth-Parthsarathy <jparthaa@gmail.com>
2023-04-17 20:52:48 +0530

    Changed input background to support DarkReader

--- src/app/components/HomeSearch.js
+++ src/app/components/HomeSearch.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import {AiOutlineSearch} from "react-icons/ai"
-import {BsFillMicFill} from "react-icons/bs"
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
 
 export default function HomeSearch() {
   return (
     <>
+      <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:ma-w-2xl">
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input type="text" className="flex-grow focus:outline-none bg-white" />
+        <BsFillMicFill className="text-lg" />
+      </form>
 
-    <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:ma-w-2xl">
-    <AiOutlineSearch className="text-xl text-gray-500 mr-3"/>
-    <input type="text" className="flex-grow focus:outline-none"/>
-    <BsFillMicFill className="text-lg"/>
-
-    </form>
-
-        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
-            <button  className="btn">Google Search</button>
-            <button className="btn">I am Feeling Lucky</button>
-        </div>
-      
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
+        <button className="btn">Google Search</button>
+        <button className="btn">I am Feeling Lucky</button>
+      </div>
     </>
-  )
+  );
 }
